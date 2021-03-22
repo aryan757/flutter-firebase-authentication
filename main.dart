c61ae4abc4a7024b17ae5dac686802ceeca16cd0
aryan757 <57763333+aryan757@users.noreply.github.com>
2021-03-22 15:02:17 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app111/src/app.dart';
+ void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
